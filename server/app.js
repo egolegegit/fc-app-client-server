0ab9532f03bd1bf98ec43a7bb5b51f5cb19f7bc1
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-02-08 11:49:47 +0700

    feat(project): init

---
+++ server/app.js
@@ -0,0 +1,15 @@
+﻿const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const chalk = require("chalk");
+
+const PORT = config.get("port") && 8080;
+
+if (process.env.NODE_ENV === "production") {
+    console.log("Production");
+}
+
+const app = express();
+app.listen(8080, () => {
+    console.log(chalk.green(`Server has been  starter on port ${PORT}`));
+});
