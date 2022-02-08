e7972924e073560b899c2fe08972b487dcbfc841
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-02-08 14:46:51 +0700

    feat(project): 8. Заносим первоначальные данные

--- server/app.js
+++ server/app.js
@@ -2,14 +2,36 @@
 const mongoose = require("mongoose");
 const config = require("config");
 const chalk = require("chalk");
+const initDatabase = require("./startup/initDatabase");
 
 const PORT = config.get("port") && 8080;
 
-if (process.env.NODE_ENV === "production") {
-    console.log("Production");
-}
+// if (process.env.NODE_ENV === "production") {
+//     console.log("Production");
+// }
 
 const app = express();
-app.listen(8080, () => {
-    console.log(chalk.green(`Server has been  starter on port ${PORT}`));
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+async function start() {
+    try {
+        await mongoose.connection.once("open", () => {
+            initDatabase();
+        });
+
+        await mongoose.connect(config.get("mongoUrl"));
+        console.log(chalk.green("MongoDB connected"));
+
+        app.listen(8080, () => {
+            console.log(
+                chalk.green(`Server has been  starter on port ${PORT}`)
+            );
+        });
+    } catch (error) {
+        console.log(chalk.red(error.message));
+        process.exit(1);
+    }
+}
+
+start().then(() => {});
---
+++ server/startup/initDatabase.js
@@ -0,0 +1,37 @@
+﻿// 1. У любого пользователей будет набор данных qualities & professions
+// 2. Они равны mock данным
+
+const Profession = require("../models/Professions");
+const Qualities = require("../models/Qualities");
+const professionMock = require("../mock/professions.json");
+const qualitiesMock = require("../mock/qualities.json");
+
+module.exports = async () => {
+  const professions = await Profession.find();
+  if (professions.length !== professionMock.length) {
+    await createInitialEntity(Profession, professionMock);
+  }
+
+  const qualities = await Qualities.find();
+  if (qualities.length !== qualitiesMock.length) {
+    await createInitialEntity(Qualities, qualitiesMock);
+  }
+};
+
+async function createInitialEntity(Model, data) {
+  await Model.collection.drop();
+
+  return Promise.all(
+    data.map(async (item) => {
+      try {
+        delete item._id;
+        const newItem = new Model(item);
+        await newItem.save();
+        return newItem;
+      } catch (error) {
+        console.log(error);
+        return error;
+      }
+    })
+  );
+}
