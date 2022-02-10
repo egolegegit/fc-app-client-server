4eab7d9891f8703342e86ec9c562fbc8ef2a5f06
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-02-10 22:25:36 +0700

    feat(project): 7. Создаем Backend. Часть 3 / 3. Создаем функционал для комментариев

--- server/routes/comment.routers.js
+++ server/routes/comment.routers.js
@@ -1,7 +1,49 @@
 ﻿const express = require("express");
+const Comment = require("../models/Comment");
+const auth = require("../middleware/auth.middleware");
 const router = express.Router({ mergeParams: true });
 
-router.get("/comment", async (req, res) => {});
-router.post("/signInWithPassword", async (req, res) => {});
+// /api/comment
+router
+  .route("/")
+  .get(auth, async (req, res) => {
+    try {
+      const { orderBy, equalTo } = req.query;
+      const list = await Comment.find({ [orderBy]: equalTo });
+      res.send(list);
+    } catch (e) {
+      res.status(500).json({ message: "Server: error!" });
+    }
+  })
+  .post(auth, async (req, res) => {
+    try {
+      const newComment = await Comment.create({
+        ...req.body,
+        userId: req.user._id
+      });
+
+      res.status(201).send(newComment);
+    } catch (e) {
+      res.status(500).json({ message: "Server: error!" });
+    }
+  });
+
+router.delete("/:commentId", auth, async (req, res) => {
+  try {
+    const { commentId } = req.params;
+
+    // const removedComment = await Comment.find({ _id: commentId });
+    const removedComment = await Comment.findById(commentId);
+
+    if (removedComment.userId.toString() === req.user._id) {
+      await removedComment.remove();
+      res.send(null);
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  } catch (e) {
+    res.status(500).json({ message: "Server: error!" });
+  }
+});
 
 module.exports = router;
