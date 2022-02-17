d511fa8ee12d44b8e39694c4fd1c46471f1cd443
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-02-18 04:03:19 +0700

    fix(project): 8. Интеграция Backend & Frontend / 4. Запускаем приложение в Docker контейнере deploy on heroku

--- client/src/app/services/baseurl.js
+++ client/src/app/services/baseurl.js
@@ -1,5 +1,7 @@
 import config from "../config/config.json";
 
+console.log("process.env.API_ENDPOINT", process.env.API_ENDPOINT);
+
 const baseurl = process.env.API_ENDPOINT || config.apiEndpoint;
 
 export default baseurl;
