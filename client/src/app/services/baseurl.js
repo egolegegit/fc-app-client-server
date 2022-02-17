02a75ae967a163deae754a9b3e722207eb427c58
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-02-18 04:03:39 +0700

    Revert "fix(project): 8. Интеграция Backend & Frontend / 4. Запускаем приложение в Docker контейнере"
    This reverts commit d511fa8ee12d44b8e39694c4fd1c46471f1cd443.

--- client/src/app/services/baseurl.js
+++ client/src/app/services/baseurl.js
@@ -1,7 +1,5 @@
 import config from "../config/config.json";
 
-console.log("process.env.API_ENDPOINT", process.env.API_ENDPOINT);
-
 const baseurl = process.env.API_ENDPOINT || config.apiEndpoint;
 
 export default baseurl;
