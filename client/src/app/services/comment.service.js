c246cb5127cfaf6729b19e873bf4ee0fe543c65e
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-02-20 23:35:43 +0700

    fix(project): 8. Интеграция Backend & Frontend / 4. Запускаем приложение в Docker контейнере deploy on heroku fix baseurl process.env.BASE_URL on heroku

--- client/src/app/services/comment.service.js
+++ client/src/app/services/comment.service.js
@@ -1,6 +1,6 @@
 import httpService from "./http.service";
 
-const commentEndpoint = "/comment/";
+const commentEndpoint = "comment/";
 
 const commentService = {
     createComment: async (payload) => {
