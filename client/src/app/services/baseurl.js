15a56dd0af5447336632d493d3b1bb05be093724
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-02-20 21:32:30 +0700

    fix(project): 8. Интеграция Backend & Frontend / 4. Запускаем приложение в Docker контейнере deploy on heroku fix baseurl process.env.BASE_URL on heroku

--- client/src/app/services/baseurl.js
+++ client/src/app/services/baseurl.js
@@ -2,7 +2,8 @@ import config from "../config/config.json";
 
 let baseurl;
 if (process.env.NODE_ENV === "production") {
-    baseurl = "https://fc-client-server-docker.herokuapp.com/api";
+    baseurl = process.env.BASE_URL;
+    // baseurl = "https://fc-client-server-docker.herokuapp.com/api";
 } else {
     baseurl = config.apiEndpoint;
 }
